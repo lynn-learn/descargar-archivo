2e9d7def350d472a72099b0d1bc8996311a7102c
lynn-learn <77292354+lynn-learn@users.noreply.github.com>
2021-01-18 14:48:33 -0500

    Add files via upload

---
+++ 1_1.php
@@ -0,0 +1,12 @@
+
+
+descargando el link, gracias por visitarnos
+
+<a  id='download' href="mysql_resources/bill.pdf" download></a>
+
+<script>
+ var down = document.getElementById('download');
+ if(down){
+    down.click();
+ }
+</script>
